b0462b6e49bb0ec7827191ddfc040209ff73f814
alfredzero <amezcuaalfredo81@gmail.com>
2024-05-08 23:32:07 -0700

    string concatenation in objc

--- objcWorld/ViewController.m
+++ objcWorld/ViewController.m
@@ -18,6 +18,21 @@
     // Do any additional setup after loading the view.
     
     _myNameLabel.text = @"Alfred0";
+    
+    NSString* stringOne =@"Swift";
+    NSString* stringTwo = @"or";
+    NSString* stringThree = @"ObjC";
+    
+    NSLog(@"%@ %@ %@", stringOne, stringTwo, stringThree);
+    
+    NSString* resString = [NSString stringWithFormat:@"%@ %@ %@", stringOne, stringTwo, stringThree];
+    NSLog(@"Second method: %@", resString);
+    
+    NSString *thirdResString = [[[stringOne stringByAppendingString:@" "] stringByAppendingString:stringTwo] stringByAppendingString:[@" " stringByAppendingString:stringThree]];
+
+    // Print the result
+    NSLog(@"Third method: %@", thirdResString);
+
 }
 
 
